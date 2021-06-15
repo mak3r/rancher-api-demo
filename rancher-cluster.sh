e4788191391268271cebf9e02d3098b9cf5d16da
Mark Abrams <mabrams@helpguest.com>
2021-06-15 15:28:02 -0400

    updated to run sourced and with proper agent command selection

--- rancher-cluster.sh
+++ rancher-cluster.sh
@@ -37,96 +37,100 @@ function usage() {
 	echo "\t-z <admin-password> - setup the admin user in rancher (ignore any -p or -u arguments)"
 }
 
-while getopts cdehikn:p:s:t:u:wxz: flag
-do
-    case "${flag}" in
-		c) ROLEFLAGS=$ROLEFLAGS" --controlplane";;
-		d) [ -n "$CLUSTER_CREATE_TYPE" ] && usage || CLUSTER_CREATE_TYPE=$DOCKER;;
-		e) ROLEFLAGS=$ROLEFLAGS" --etcd";;
-        h) usage
-			exit 0
-			;;
-		i) INSECURE="--insecure";;
-		k) [ -n "$CLUSTER_CREATE_TYPE" ] && usage || CLUSTER_CREATE_TYPE=$KUBERNETES;;
-		n) CLUSTERNAME=$OPTARG;;
-		p) PASSWORD=$OPTARG;;
-		s) RANCHER_SERVER=$OPTARG;;
-		t) APITOKEN=$OPTARG;;
-		u) USER=$OPTARG;;
-		w) ROLEFLAGS=$ROLEFLAGS" --worker";;
-		x) EXECUTE="true";;
-		z) ADMIN_PASSWORD=$OPTARG
-			USER="admin"
-			[ ! -n $PASSWORD ] && : || PASSWORD="admin"
-			;;
-    esac
-done
+function create_cluster() {
+	OPTIND=1
+	set -x
+	while getopts cdehikn:p:s:t:u:wxz: flag
+	do
+		case "${flag}" in
+			c) ROLEFLAGS=$ROLEFLAGS" --controlplane";;
+			d) [ -n "$CLUSTER_CREATE_TYPE" ] && usage || CLUSTER_CREATE_TYPE=$DOCKER;;
+			e) ROLEFLAGS=$ROLEFLAGS" --etcd";;
+			h) usage
+				exit 0
+				;;
+			i) INSECURE="--insecure";;
+			k) [ -n "$CLUSTER_CREATE_TYPE" ] && usage || CLUSTER_CREATE_TYPE=$KUBERNETES;;
+			n) CLUSTERNAME=$OPTARG;;
+			p) PASSWORD=$OPTARG;;
+			s) RANCHER_SERVER=$OPTARG;;
+			t) APITOKEN=$OPTARG;;
+			u) USER=$OPTARG;;
+			w) ROLEFLAGS=$ROLEFLAGS" --worker";;
+			x) EXECUTE="true";;
+			z) ADMIN_PASSWORD=$OPTARG
+				USER="admin"
+				[ ! -n $PASSWORD ] && : || PASSWORD="admin"
+				;;
+		esac
+	done
 
-if [ -z "$CLUSTER_CREATE_TYPE" ]; then
-	echo "ERROR: you must specify one of -d or -k"
-	usage
-	exit 1
-fi
+	if [ -z "$CLUSTER_CREATE_TYPE" ]; then
+		echo "ERROR: you must specify one of -d or -k"
+		usage
+		exit 1
+	fi
 
-if [ -z "$APITOKEN" ]; then
-	# Login
-	LOGINRESPONSE=`curl -s "$RANCHER_SERVER/v3-public/localProviders/local?action=login" -H 'content-type: application/json' --data-binary '{"username":"'$USER'","'password'":"'$PASSWORD'"}' $INSECURE`
-	LOGINTOKEN=`echo $LOGINRESPONSE | jq -r .token`
-
-	if [ -n "$ADMIN_PASSWORD" ]; then
-		# Change password
-		curl -s $RANCHER_SERVER'/v3/users?action=changepassword' -H 'content-type: application/json' -H "Authorization: Bearer $LOGINTOKEN" --data-binary '{"currentPassword":"'$PASSWORD'","newPassword":"'$ADMIN_PASSWORD'"}' $INSECURE
-
-		# Get a new token
-		LOGINRESPONSE=`curl -s "$RANCHER_SERVER/v3-public/localProviders/local?action=login" -H 'content-type: application/json' --data-binary '{"username":"'$USER'","'password'":"'$ADMIN_PASSWORD'"}' $INSECURE`
+	if [ -z "$APITOKEN" ]; then
+		# Login
+		LOGINRESPONSE=`curl -s "$RANCHER_SERVER/v3-public/localProviders/local?action=login" -H 'content-type: application/json' --data-binary '{"username":"'$USER'","'password'":"'$PASSWORD'"}' $INSECURE`
 		LOGINTOKEN=`echo $LOGINRESPONSE | jq -r .token`
+
+		if [ -n "$ADMIN_PASSWORD" ]; then
+			# Change password
+			curl -s $RANCHER_SERVER'/v3/users?action=changepassword' -H 'content-type: application/json' -H "Authorization: Bearer $LOGINTOKEN" --data-binary '{"currentPassword":"'$PASSWORD'","newPassword":"'$ADMIN_PASSWORD'"}' $INSECURE
+
+			# Get a new token
+			LOGINRESPONSE=`curl -s "$RANCHER_SERVER/v3-public/localProviders/local?action=login" -H 'content-type: application/json' --data-binary '{"username":"'$USER'","'password'":"'$ADMIN_PASSWORD'"}' $INSECURE`
+			LOGINTOKEN=`echo $LOGINRESPONSE | jq -r .token`
+		fi
+
+		# Create API key
+		APIRESPONSE=`curl -s "$RANCHER_SERVER/v3/token" -H 'content-type: application/json' -H "Authorization: Bearer $LOGINTOKEN" --data-binary '{"type":"token","description":"'$CLUSTERNAME'"}' $INSECURE`
+		# Extract and store token
+		APITOKEN=`echo $APIRESPONSE | jq -r .token`
+
+		if [ -n "$ADMIN_PASSWORD" ]; then
+			# Set server-url
+			curl -s $RANCHER_SERVER'/v3/settings/server-url' -H 'content-type: application/json' -H "Authorization: Bearer $APITOKEN" -X PUT --data-binary '{"name":"server-url","value":"'$RANCHER_SERVER'"}' $INSECURE > /dev/null
+
+		fi
 	fi
 
-	# Create API key
-	APIRESPONSE=`curl -s "$RANCHER_SERVER/v3/token" -H 'content-type: application/json' -H "Authorization: Bearer $LOGINTOKEN" --data-binary '{"type":"token","description":"'$CLUSTERNAME'"}' $INSECURE`
-	# Extract and store token
-	APITOKEN=`echo $APIRESPONSE | jq -r .token`
 
-	if [ -n "$ADMIN_PASSWORD" ]; then
-		# Set server-url
-		curl -s $RANCHER_SERVER'/v3/settings/server-url' -H 'content-type: application/json' -H "Authorization: Bearer $APITOKEN" -X PUT --data-binary '{"name":"server-url","value":"'$RANCHER_SERVER'"}' $INSECURE > /dev/null
+	# Create token
+	#curl -s "$RANCHER_SERVER/v3/clusterregistrationtoken" -H 'content-type: application/json' -H "Authorization: Bearer $APITOKEN" --data-binary '{"type":"clusterRegistrationToken","clusterId":"'$CLUSTERID'"}' $INSECURE > /dev/null
 
-	fi
-fi
+	if [ "$CLUSTER_CREATE_TYPE" == "$DOCKER" ]; then
+		# Create cluster
+		CLUSTERRESPONSE=`curl -s "$RANCHER_SERVER/v3/cluster?_replace=true" -H 'content-type: application/json' -H "Authorization: Bearer $APITOKEN" --data-binary '{"dockerRootDir":"/var/lib/docker","enableNetworkPolicy":false,"type":"cluster","rancherKubernetesEngineConfig":{"addonJobTimeout":30,"ignoreDockerVersion":true,"sshAgentAuth":false,"type":"rancherKubernetesEngineConfig","authentication":{"type":"authnConfig","strategy":"x509"},"network":{"type":"networkConfig","plugin":"canal"},"ingress":{"type":"ingressConfig","provider":"nginx"},"monitoring":{"type":"monitoringConfig","provider":"metrics-server"},"services":{"type":"rkeConfigServices","kubeApi":{"podSecurityPolicy":false,"type":"kubeAPIService"},"etcd":{"snapshot":false,"type":"etcdService","extraArgs":{"heartbeat-interval":500,"election-timeout":5000}}}},"name":"'$CLUSTERNAME'"}' $INSECURE`
+		# Extract clusterid to use for generating the run command
+		CLUSTERID=`echo $CLUSTERRESPONSE | jq -r .id`
 
-
-# Create token
-#curl -s "$RANCHER_SERVER/v3/clusterregistrationtoken" -H 'content-type: application/json' -H "Authorization: Bearer $APITOKEN" --data-binary '{"type":"clusterRegistrationToken","clusterId":"'$CLUSTERID'"}' $INSECURE > /dev/null
-
-if [ "$CLUSTER_CREATE_TYPE" == "$DOCKER" ]; then
-	# Create cluster
-	CLUSTERRESPONSE=`curl -s "$RANCHER_SERVER/v3/cluster?_replace=true" -H 'content-type: application/json' -H "Authorization: Bearer $APITOKEN" --data-binary '{"dockerRootDir":"/var/lib/docker","enableNetworkPolicy":false,"type":"cluster","rancherKubernetesEngineConfig":{"addonJobTimeout":30,"ignoreDockerVersion":true,"sshAgentAuth":false,"type":"rancherKubernetesEngineConfig","authentication":{"type":"authnConfig","strategy":"x509"},"network":{"type":"networkConfig","plugin":"canal"},"ingress":{"type":"ingressConfig","provider":"nginx"},"monitoring":{"type":"monitoringConfig","provider":"metrics-server"},"services":{"type":"rkeConfigServices","kubeApi":{"podSecurityPolicy":false,"type":"kubeAPIService"},"etcd":{"snapshot":false,"type":"etcdService","extraArgs":{"heartbeat-interval":500,"election-timeout":5000}}}},"name":"'$CLUSTERNAME'"}' $INSECURE`
-	# Extract clusterid to use for generating the run command
-	CLUSTERID=`echo $CLUSTERRESPONSE | jq -r .id`
-
-	# Fetch docker command
-	AGENTCMD=`curl -s $RANCHER_SERVER'/v3/clusterregistrationtokens?id="'$CLUSTERID'"&limit=-1' -H 'content-type: application/json' -H "Authorization: Bearer $APITOKEN" $INSECURE | jq -r '.data[].nodeCommand' | head -1`
-else
-	# It's not a custom cluster so let's create an import cluster
-	# Create cluster
-	CLUSTERRESPONSE=`curl -s "$RANCHER_SERVER/v3/cluster?_replace=true" -H 'content-type: application/json' -H "Authorization: Bearer $APITOKEN" --data-binary '{"name":"'$CLUSTERNAME'"}' $INSECURE`
-	# Extract clusterid to use for generating the run command
-	CLUSTERID=`echo $CLUSTERRESPONSE | jq -r .id`
-
-	if [ -z "$INSECURE" ]; then
-		# Fetch k8s command
-		AGENTCMD=`curl -s $RANCHER_SERVER'/v3/clusterregistrationtokens?id="'$CLUSTERID'"&limit=-1' -H 'content-type: application/json' -H "Authorization: Bearer $APITOKEN" -X POST --data-binary '{"clusterId":"'$CLUSTERID'","name":"'$CLUSTERNAME'"}' | jq -r '.command' `
+		# Fetch docker command
+		AGENTCMD=`curl -s $RANCHER_SERVER'/v3/clusterregistrationtokens?id="'$CLUSTERID'"&limit=-1' -H 'content-type: application/json' -H "Authorization: Bearer $APITOKEN" $INSECURE | jq -r '.data[].nodeCommand' | head -1`
 	else
-		# Fetch insecure k8s command
-		AGENTCMD=`curl -s $RANCHER_SERVER'/v3/clusterregistrationtokens?id="'$CLUSTERID'"&limit=-1' -H 'content-type: application/json' -H "Authorization: Bearer $APITOKEN" -X POST --data-binary '{"clusterId":"'$CLUSTERID'","name":"'$CLUSTERNAME'"}' $INSECURE | jq -r '.insecureCommand' `
+		# It's not a custom cluster so let's create an import cluster
+		# Create cluster
+		CLUSTERRESPONSE=`curl -s "$RANCHER_SERVER/v3/cluster?_replace=true" -H 'content-type: application/json' -H "Authorization: Bearer $APITOKEN" --data-binary '{"name":"'$CLUSTERNAME'"}' $INSECURE`
+		# Extract clusterid to use for generating the run command
+		CLUSTERID=`echo $CLUSTERRESPONSE | jq -r .id`
+
+		if [ -z "$INSECURE" ]; then
+			# Fetch k8s command
+			AGENTCMD=`curl -s $RANCHER_SERVER'/v3/clusterregistrationtokens?id="'$CLUSTERID'"&limit=-1' -H 'content-type: application/json' -H "Authorization: Bearer $APITOKEN" | jq -r '.data[0].command' `
+		else
+			# Fetch insecure k8s command
+			AGENTCMD=`curl -s $RANCHER_SERVER'/v3/clusterregistrationtokens?id="'$CLUSTERID'"&limit=-1' -H 'content-type: application/json' -H "Authorization: Bearer $APITOKEN" $INSECURE | jq -r '.data[0].insecureCommand' `
+		fi
 	fi
-fi
 
-# Concat commands
-RUNCMD="$AGENTCMD $ROLEFLAGS"
+	# Concat commands
+	RUNCMD="$AGENTCMD $ROLEFLAGS"
 
-if [ -n "$EXECUTE" ]; then 
-	eval $RUNCMD 
-else
-	echo $RUNCMD
-fi
+	if [ -n "$EXECUTE" ]; then 
+		eval $RUNCMD 
+	else
+		echo $RUNCMD
+	fi
+}
